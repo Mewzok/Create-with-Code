689820d7e6ab81ca16d0e014a66b10df0bba9ad8
404kevnotfound <student@CS-Mac-mini-18.local>
2025-09-09 14:37:13 -0600

    -Complete Lesson 3.2

--- Prototype 3/Assets/Scripts/MoveLeft.cs
+++ Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -3,16 +3,26 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 30;
+    private PlayerController playerControllerScript;
+    private float leftBound = -15;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        // move object left while not game over
+        if(playerControllerScript.gameOver == false) {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        // delete obstacles when off screen
+        if(transform.position.x < leftBound && gameObject.CompareTag("Obstacle")) {
+            Destroy(gameObject);
+        }
     }
 }
